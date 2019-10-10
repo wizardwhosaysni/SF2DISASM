f8e5ac859e3fe6e07da6eccda8bad1aba084b066
Wiz <the_wizard_who_says_ni@hotmail.com>
2019-10-10 23:00:19 +0200

    Found a way to properly remove IAN from battle 01.

--- disasm/data/battles/entries/sffc-battle01/cs_afterbattle.asm
+++ disasm/data/battles/entries/sffc-battle01/cs_afterbattle.asm
@@ -15,73 +15,14 @@ abcs_battle01:  textCursor $901
                 setF $192
                 warp 1,5,5,DOWN
                 yesNo
+                executeSubroutine reinsertIanIntoParty
                 csc_end        
-                csWait 40
-                shiver 128
-                csWait 30
-                shiver 128
-                csWait 30
-                mapFadeOutToWhite
-                csWait 30
-                mapFadeInFromWhite
-                csWait 5
-                setActscript 129,eas_461B6
-                csWait 5
-                setActscript ALLY_IAN,eas_461B6
-                csWait 30
-                mapFadeOutToWhite
-                csWait 30
-                mapFadeInFromWhite
-                csWait 30
-                mapFadeOutToWhite
-                animEntityFX 128,6
-                setSprite 128,MAPSPRITE_BLANK
-                setPos 128,7,10,DOWN
-                setSprite 128,MAPSPRITE_MIST_DEMON
-                mapFadeInFromWhite
-                csWait 30
-                entityActionsWait 129
-                 moveUp 1
-                endActions
-                nextSingleText $0,129   ; "Oh, it disappeared?!{W1}"
-                csWait 5
-                setActscript 129,eas_461B6
-                csWait 120
-                entityActionsWait 129
-                 moveDown 1
-                endActions
-                nextSingleText $0,129   ; "Oh, there it is!{W1}"
-                setFacing ALLY_IAN,DOWN
-                setFacing ALLY_MEAD,DOWN
-                setFacing ALLY_PAIGE,DOWN
-                setCamDest 2,3
-                csWait 70
-                mapFadeOutToWhite
-                animEntityFX 128,6
-                setSprite 128,MAPSPRITE_BLANK
-                setPos 128,7,16,DOWN
-                setSprite 128,MAPSPRITE_MIST_DEMON
-                mapFadeInFromWhite
-                csWait 30
-                nextSingleText $0,129   ; "It disappeared again.{W1}"
-                setCamDest 2,10
-                nextSingleText $0,129   ; "There it is!{W1}"
-                entityActions 128
-                 moveDown 4
-                endActions
-                animEntityFX 128,6
-                csWait 20
-                hide 128
-                setCamDest 2,2
-                setFacing 129,RIGHT
-                nextSingleText $0,129   ; "We must hurry to the King!{W1}"
-                entityActionsWait 129
-                 moveDown 6
-                endActions
-                hide 129
-                csWait 40
-                csc_end
 ce_497F6:       mainEntity 8,6,UP
                 entity 7,4,DOWN,MAPSPRITE_MIST_DEMON,eas_Init
                 entity 7,6,RIGHT,MAPSPRITE_ASTRAL,eas_Init
                 dc.w $FFFF
+                
+reinsertIanIntoParty:
+                moveq   #ALLY_IAN,d0  ; starting character
+                jsr   JoinForce
+                rts                
--- disasm/data/battles/entries/sffc-battle01/cs_beforebattle.asm
+++ disasm/data/battles/entries/sffc-battle01/cs_beforebattle.asm
@@ -15,121 +15,9 @@ bbcs_01:        textCursor $8F4
                 nextText $0,135         ; "There you are, {LEADER}.{N}Stay here.{W2}"
                 nextSingleText $0,135   ; "I'll take a look inside the{N}tower.{W1}"
                 setCameraEntity 135
+                executeSubroutine removeIanFromParty
                 csc_end                    
-                entityActionsWait 135
-                 moveUp 1
-                endActions
-                csWait 5
-                setActscript 135,eas_461B6
-                csWait 60
-                nextSingleText $0,135   ; "Hmm, what a mysterious{N}tower.  I wonder who built{N}it?{W1}"
-                entityActionsWait 135
-                 moveUp 1
-                 eaWait 60
-                 moveUp 1
-                endActions
-                csWait 50
-                nextSingleText $0,135   ; "I have a strange feeling{N}about this place.{W1}"
-                entityActionsWait 135
-                 moveUp 3
-                endActions
-                nextSingleText $0,135   ; "Oh, what's this?{N}I think I have seen this{N}symbol before....{W1}"
-                csWait 40
-                mapFadeOutToWhite
-                csWait 30
-                mapFadeInFromWhite
-                csWait 30
-                mapFadeOutToWhite
-                csWait 30
-                mapFadeInFromWhite
-                csWait 5
-                setActscript 135,eas_461B6
-                csWait 30
-                mapFadeOutToWhite
-                csWait 30
-                mapFadeInFromWhite
-                csWait 30
-                setCamDest 2,10
-                csWait 5
-                setActscript ALLY_PAIGE,eas_461B6
-                csWait 60
-                nextSingleText $0,ALLY_PAIGE ; "What's happening?{W1}"
-                entityActionsWait ALLY_MEAD
-                 moveUp 1
-                endActions
-                nextSingleText $0,ALLY_MEAD ; "Look!  Something is{N}appearing.  What's that?{W1}"
-                setFacing 135,UP
-                setCamDest 2,1
-                setPos 128,7,4,DOWN
-                animEntityFX 128,7
-                setActscriptWait 128,eas_46172
-                setActscriptWait 135,eas_46172
-                entityActionsWait 135
-                 moveDown 1
-                endActions
-                nextText $0,135         ; "An evil Gizmo!  A devil{N}that possesses people!{W2}"
-                nextSingleText $0,135   ; "Why are you here?{W1}"
-                csWait 30
-                entityActionsWait 128
-                 moveRight 1
-                endActions
-                entityActionsWait 135
-                 moveRight 1
-                endActions
-                nextSingleText $0,135   ; "Where are you going?{N}What...wait!  You're here to{N}possess the King, aren't you?{W1}"
-                entityActionsWait 128
-                 moveLeft 1
-                endActions
-                entityActionsWait 135
-                 moveLeft 1
-                endActions
-                nextSingleText $0,135   ; "I won't let you pass.{N}You shall not harm the King!{W1}"
-                shiver 128
-                csWait 30
-                shiver 128
-                setPos 129,7,4,DOWN
-                setPos 130,7,4,DOWN
-                setPos 131,7,4,DOWN
-                setPos 132,7,4,DOWN
-                setPos 133,7,4,DOWN
-                setPos 134,7,4,DOWN
-                setActscriptWait 129,eas_46172
-                setActscriptWait 130,eas_46172
-                setActscriptWait 131,eas_46172
-                setActscriptWait 132,eas_46172
-                setActscriptWait 133,eas_46172
-                setActscriptWait 134,eas_46172
-                entityActions 129
-                 moveUp 1
-                endActions
-                entityActions 130
-                 moveLeft 1
-                endActions
-                entityActions 131
-                 moveDown 1
-                 moveLeft 1
-                endActions
-                entityActions 132
-                 moveRight 2
-                 moveUp 1
-                endActions
-                entityActions 133
-                 moveRight 3
-                endActions
-                entityActions 134
-                 moveRight 3
-                 moveDown 1
-                endActions
-                shiver 128
-                csWait 30
-                shiver 128
-                csWait 30
-                shiver 128
-                csWait 60
-                nextText $0,135         ; "This doesn't look good.{W2}"
-                setFacing 135,DOWN
-                nextSingleText $0,135   ; "We'll stop the evil Gizmo{N}here.  {LEADER}, defeat{N}these fiends!{W1}"
-                csc_end
+ 
 ce_49694:       mainEntity 7,16,UP
                 entity 63,63,DOWN,MAPSPRITE_MIST_DEMON,eas_Init
                 entity 63,63,DOWN,MAPSPRITE_GIZMO,eas_Init
@@ -140,3 +28,8 @@ ce_49694:       mainEntity 7,16,UP
                 entity 63,63,DOWN,MAPSPRITE_GIZMO,eas_Init
                 entity 7,11,UP,MAPSPRITE_ASTRAL,eas_Init
                 dc.w $FFFF
+
+removeIanFromParty:
+                moveq   #ALLY_IAN,d0  ; starting character
+                jsr   LeaveForce
+                rts
