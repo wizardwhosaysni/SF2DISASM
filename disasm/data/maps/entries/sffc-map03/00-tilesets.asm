547e3a532c76c86e5521cfcf3699efe68815347e
Wiz <the_wizard_who_says_ni@hotmail.com>
2019-11-28 22:23:13 +0100

    Replaced original map assets with properly initialized new entry lists.

---
+++ disasm/data/maps/entries/sffc-map03/00-tilesets.asm
@@ -0,0 +1,9 @@
+
+; ASM FILE data\maps\entries\map78\00-tilesets.asm :
+; 0xC6990..0xC6996 : 
+                mapPalette  2
+                mapTileset1 15
+                mapTileset2 16
+                mapTileset3 17
+                mapTileset4 255
+                mapTileset5 255
