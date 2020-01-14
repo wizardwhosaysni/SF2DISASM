eae244ad4ce732f9ef0f8c8bf34e651019af27c7
Wiz <the_wizard_who_says_ni@hotmail.com>
2020-01-15 00:09:47 +0100

    Updated battle spritesets according to new enemy list.

--- disasm/data/battles/global/battlemapcoords.asm
+++ disasm/data/battles/global/battlemapcoords.asm
@@ -7,24 +7,22 @@ BattleMapCoordinates:
                 battleMapCoords 1, 0, 0, 25, 33, 255, 255
                 battleMapCoords 2, 0, 0, 32, 25, 255, 255
                 battleMapCoords 3, 0, 0, 24, 32, 255, 255
-                battleMapCoords 4, 0, 0, 25, 33, 255, 255
-                battleMapCoords 5, 0, 0, 25, 33, 255, 255
-                battleMapCoords 6, 0, 0, 32, 25, 255, 255
-                battleMapCoords 7, 0, 0, 24, 32, 255, 255
-                battleMapCoords 8, 0, 0, 25, 33, 255, 255
-                battleMapCoords 9, 0, 0, 25, 33, 255, 255
-                battleMapCoords 10,0, 0, 32, 25, 255, 255
-                battleMapCoords 11, 0, 0, 24, 32, 255, 255
-                battleMapCoords 12, 0, 0, 25, 33, 255, 255
-                battleMapCoords 13, 0, 0, 25, 33, 255, 255
-                battleMapCoords 14, 0, 0, 32, 25, 255, 255
-                battleMapCoords 15, 0, 0, 24, 32, 255, 255
-                battleMapCoords 16, 0, 0, 25, 33, 255, 255
-                battleMapCoords 17, 0, 0, 25, 33, 255, 255
-                battleMapCoords 18, 0, 0, 32, 25, 255, 255
+                battleMapCoords 4, 0, 0, 22, 32, 255, 255
+                battleMapCoords 5, 0, 0, 23, 31, 255, 255
+                battleMapCoords 6, 0, 0, 28, 24, 255, 255
+                battleMapCoords 7, 0, 0, 21, 32, 255, 255
+                battleMapCoords 8, 0, 0, 24, 22, 255, 255
+                battleMapCoords 9, 0, 0, 24, 32, 255, 255
+                battleMapCoords 10, 0, 0, 32, 24, 255, 255
+                battleMapCoords 11, 0, 0, 24, 24, 255, 255
+                battleMapCoords 12, 0, 0, 18, 31, 255, 255
+                battleMapCoords 13, 0, 0, 32, 24, 255, 255
+                battleMapCoords 14, 0, 0, 29, 32, 255, 255
+                battleMapCoords 15, 0, 0, 23, 29, 255, 255
+                battleMapCoords 16, 0, 0, 28, 32, 255, 255
+                battleMapCoords 17, 0, 0, 24, 32, 255, 255
+                battleMapCoords 18, 0, 0, 19, 31, 255, 255
                 battleMapCoords 19, 0, 0, 24, 32, 255, 255
-                battleMapCoords 20, 0, 0, 25, 33, 255, 255
-                battleMapCoords 21, 0, 0, 25, 33, 255, 255
-                battleMapCoords 22, 0, 0, 32, 25, 255, 255
-                
-                align 2
+                battleMapCoords 20, 0, 0, 24, 32, 255, 255
+                battleMapCoords 21, 0, 0, 19, 31, 255, 255
+                battleMapCoords 22, 0, 0, 19, 17, 255, 255
